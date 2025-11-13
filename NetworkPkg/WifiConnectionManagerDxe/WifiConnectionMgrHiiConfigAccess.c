f5c82adadd208fde3a289eaff360df901b2df59d
Mike Beaton <mjsbeaton@gmail.com>
2025-11-13 20:45:47 +0000

    Revert "NetworkPkg: Remove unused variable"
    This reverts commit 47ead0ea7de1733137fca9733cb27087c61c9ecc.

--- NetworkPkg/WifiConnectionManagerDxe/WifiConnectionMgrHiiConfigAccess.c
+++ NetworkPkg/WifiConnectionManagerDxe/WifiConnectionMgrHiiConfigAccess.c
@@ -390,6 +390,7 @@ WifiMgrRefreshNetworkList (
 {
   EFI_STATUS                Status;
   EFI_TPL                   OldTpl;
+  UINT32                    AvailableCount;
   EFI_STRING_ID             PortPromptToken;
   EFI_STRING_ID             PortTextToken;
   EFI_STRING_ID             PortHelpToken;
@@ -417,6 +418,7 @@ WifiMgrRefreshNetworkList (
   }
 
   OldTpl            = gBS->RaiseTPL (TPL_CALLBACK);
+  AvailableCount    = 0;
   PortStringSize    = sizeof (PortString);
   ConnectedProfile  = NULL;
   AKMListDisplay    = NULL;
@@ -431,6 +433,7 @@ WifiMgrRefreshNetworkList (
         Private->CurrentNic->CurrentOperateNetwork->IsAvailable)
     {
       Profile = Private->CurrentNic->CurrentOperateNetwork;
+      AvailableCount++;
 
       AKMListDisplay = WifiMgrGetStrAKMList (Profile);
       if (AKMListDisplay == NULL) {
@@ -506,6 +509,8 @@ WifiMgrRefreshNetworkList (
     }
 
     if (Profile->IsAvailable && Profile->CipherSuiteSupported) {
+      AvailableCount++;
+
       AKMListDisplay = WifiMgrGetStrAKMList (Profile);
       if (AKMListDisplay == NULL) {
         Status = EFI_OUT_OF_RESOURCES;
@@ -593,6 +598,8 @@ WifiMgrRefreshNetworkList (
     }
 
     if (Profile->IsAvailable && !Profile->CipherSuiteSupported) {
+      AvailableCount++;
+
       AKMListDisplay = WifiMgrGetStrAKMList (Profile);
       if (AKMListDisplay == NULL) {
         Status = EFI_OUT_OF_RESOURCES;
